d719ddf38fbd8ec1efb4b1c5a775087cc286898b
asifaminisonline <asifaminisonline@gmail.com>
2023-04-28 17:44:02 +0530

    fix linter errors

--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -1,10 +1,1 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Foods', type: :request do
-#   describe 'GET /index' do
-#     it 'returns http success' do
-#       get '/foods'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+require 'rails_helper'
